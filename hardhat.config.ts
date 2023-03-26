34f64fb7c9eecb157b863683969410ada0200490
leekt <leekt216@gmail.com>
2023-03-27 01:27:16 +0900

    initialize

---
+++ hardhat.config.ts
@@ -0,0 +1,97 @@
+import { HardhatUserConfig } from "hardhat/config";
+import fs from 'fs'
+import { task } from "hardhat/config";
+import { runOp1, callData, fillUserOp, getInitCode, getSender, signUserOp, signUserOpWithPaymaster } from "./scripts/runOp";
+import "@nomiclabs/hardhat-ethers";
+import { hexlify } from "ethers/lib/utils";
+import { Signer, Wallet } from "ethers";
+require('dotenv').config();
+
+const infuraKey = process.env.INFURA_KEY;
+const mnemonicFileName = process.env.MNEMONIC_FILE
+let mnemonic = 'test '.repeat(11) + 'junk'
+if (mnemonicFileName != null && fs.existsSync(mnemonicFileName)) {
+  mnemonic = fs.readFileSync(mnemonicFileName, 'ascii').trim()
+}
+
+function getNetwork(network : string) {
+  return {
+    url: `https://${network}.infura.io/v3/${infuraKey}`,
+    accounts: mnemonic.startsWith('0x') ? [mnemonic] : {
+      mnemonic: mnemonic,
+    },
+  };
+}
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+  networks: {
+    goerli: getNetwork('goerli'),
+    arbitrum: getNetwork('arbitrum-mainnet'),
+  }
+};
+
+task('get-sender', 'Get sender address')
+  .addParam('owner', 'Owner address')
+  .addParam('nonce', 'Nonce')
+  .setAction(async (taskArgs, hre) => {
+    const sender = await getSender(hre,taskArgs.owner, taskArgs.nonce);
+    console.log("sender addr : " + sender.address);
+  });
+
+task("test-paymaster", "Test paymaster")
+  .addParam('owner', 'Owner address')
+  .addParam('nonce', 'Nonce')
+  .setAction(async (taskArgs, hre) => {
+    const sender = await getSender(hre,taskArgs.owner, taskArgs.nonce);
+    let initCode = "0x";
+    if(await hre.ethers.provider.getCode(sender.address) == '0x') {
+      console.log("Sender is not deployed");
+      initCode = await getInitCode(hre, sender.address, taskArgs.nonce);
+    }
+    const userOp = await fillUserOp(hre, {
+      sender: sender.address,
+      initCode: initCode,
+      callData: await callData(hre, sender.address, 0, "0x"),
+    });
+    let signer : Signer;
+    if(taskArgs.owner.toLowerCase() == "0xae72a48c1a36bd18af168541c53037965d26e4a8") {
+      // test account
+      signer = new Wallet('0x'.padEnd(66, '7'));
+    } else {
+      signer = hre.ethers.provider.getSigner(taskArgs.owner);
+    }
+    userOp.paymasterAndData = hexlify(await signUserOpWithPaymaster(hre, userOp));
+    userOp.signature = hexlify(await signUserOp(hre, userOp, signer));
+    await runOp1(hre, userOp);
+  });
+
+task("test-bundler", "Test paymaster")
+  .addParam('owner', 'Owner address')
+  .addParam('nonce', 'Nonce')
+  .setAction(async (taskArgs, hre) => {
+    const sender = await getSender(hre,taskArgs.owner, taskArgs.nonce);
+    console.log("sender addr : " + sender.address);
+    let initCode = "0x";
+    if(await hre.ethers.provider.getCode(sender.address) == '0x') {
+      console.log("Sender is not deployed");
+      initCode = await getInitCode(hre, taskArgs.owner, taskArgs.nonce);
+    }
+    const userOp = await fillUserOp(hre, {
+      sender: sender.address,
+      initCode: initCode,
+      callData: await callData(hre, sender.address, 0, "0x"),
+    });
+    let signer : Signer;
+    if(taskArgs.owner.toLowerCase() == "0xae72a48c1a36bd18af168541c53037965d26e4a8") {
+      // test account
+      signer = new Wallet('0x'.padEnd(66, '7'));
+    } else {
+      signer = hre.ethers.provider.getSigner(taskArgs.owner);
+    }
+    userOp.signature = hexlify(await signUserOp(hre, userOp, signer));
+    userOp.paymasterAndData = hexlify("0x");
+    await runOp1(hre, userOp);
+  });
+
+export default config;
---
+++ scripts/runOp.ts
@@ -0,0 +1,100 @@
+import { JsonRpcProvider } from "@ethersproject/providers";
+import { BigNumberish, Signer } from "ethers";
+import { SimpleAccountFactory__factory, SimpleAccountFactory, EntryPoint__factory, SimpleAccount, SimpleAccount__factory } from "@account-abstraction/contracts";
+import { arrayify, hexConcat, hexlify } from "ethers/lib/utils";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+require('dotenv').config();
+
+const PIMLICO_API_KEY = process.env.PIMLICO_API_KEY;
+const config = require('../configs/config.json');
+
+function getBundlerUrl(network : string) : string {
+  return `https://api-development.pimlico.io/v1/${network}/rpc?apikey=${PIMLICO_API_KEY}`
+}
+
+interface UserOpStruct {
+  sender: string,
+  nonce: BigNumberish,
+  initCode : string,
+  callData : string,
+  callGasLimit : BigNumberish,
+  verificationGasLimit : BigNumberish,
+  preVerificationGas : BigNumberish,
+  maxFeePerGas : BigNumberish,
+  maxPriorityFeePerGas : BigNumberish,
+  paymasterAndData : string,
+  signature : string
+}
+
+export async function runOp1(hre : HardhatRuntimeEnvironment, userOp : UserOpStruct) {
+  const bundlerProvider = new JsonRpcProvider(getBundlerUrl(hre.network.name));
+  const receipt = await bundlerProvider.send("eth_sendUserOperation", [
+    userOp,
+    config[hre.network.name].entrypoint,
+  ]);
+  console.log("Receipt: ", receipt);
+}
+
+export async function signUserOp(hre: HardhatRuntimeEnvironment, userOp : UserOpStruct, signer : Signer) : Promise<string> {
+  console.log("signer addr" + await signer.getAddress());
+  const entryPoint = EntryPoint__factory.connect(config[hre.network.name].entrypoint, hre.ethers.provider);
+  const signature = await signer.signMessage(arrayify(await entryPoint.getUserOpHash(userOp)));
+  console.log(signature);
+  return signature;
+}
+
+export async function callData(hre: HardhatRuntimeEnvironment, to : string, value: BigNumberish, data: string) : Promise<string> {
+  const account = SimpleAccount__factory.connect(config[hre.network.name].factory, hre.ethers.provider);
+  return account.interface.encodeFunctionData('execute', [to, value, data]);
+}
+
+export async function fillUserOp(hre: HardhatRuntimeEnvironment, userOp:Partial<UserOpStruct>) : Promise<UserOpStruct> {
+  const signer = hre.ethers.provider.getSigner();
+  const sender = SimpleAccount__factory.connect(userOp.sender!, signer);
+  if(await hre.ethers.provider.getCode(userOp.sender!) == '0x') {
+    userOp.nonce = hexlify(0);
+  } else {
+    userOp.nonce = hexlify((await sender.nonce()).toNumber());
+  }
+  userOp.callGasLimit = hexlify(1000000);
+  userOp.verificationGasLimit = hexlify(1000000);
+  userOp.preVerificationGas = hexlify(1000000);
+  userOp.maxFeePerGas = hexlify(100000000000);
+  userOp.maxPriorityFeePerGas = hexlify(100000000000);
+  userOp.paymasterAndData = hexlify('0x');
+  userOp.signature = hexlify('0x');
+  return userOp as UserOpStruct;
+}
+
+export async function signUserOpWithPaymaster(hre: HardhatRuntimeEnvironment, userOp : UserOpStruct) : Promise<string> {
+  const bundlerProvider = new JsonRpcProvider(getBundlerUrl(hre.network.name));
+  const signature = await bundlerProvider.send("pm_sponsorUserOperation", [
+    userOp,
+    {
+      entryPoint : config[hre.network.name].entrypoint,
+    }
+  ]);
+  console.log("Signature: ", signature.paymasterAndData);
+  return signature.paymasterAndData;
+}
+
+export function getInitCode(hre: HardhatRuntimeEnvironment, owner : string, nonce : BigNumberish) : string {
+  const factory = SimpleAccountFactory__factory.connect(config[hre.network.name].factory, hre.ethers.provider);
+  const data = hexConcat([factory.address, factory.interface.encodeFunctionData('createAccount', [owner, nonce])]);
+  return data;
+}
+
+export async function getSender(hre : HardhatRuntimeEnvironment, owner : string, nonce : BigNumberish) : Promise<SimpleAccount> {
+  const signer = hre.ethers.provider.getSigner();
+  const entryPoint = EntryPoint__factory.connect(config[hre.network.name].entrypoint, signer);
+  const initCode = getInitCode(hre, owner, nonce);
+  const sender : string = await entryPoint.getSenderAddress(initCode).then(x => {
+    throw new Error("should be reverted");
+  }).catch((e) => {
+    const data = e.message.split('0x6ca7b806')[1].split("\"")[0];
+    const addr = hre.ethers.utils.getAddress('0x' + data.slice(24, 64));
+    return addr;
+  });
+  return SimpleAccount__factory.connect(sender, hre.ethers.provider);
+}
